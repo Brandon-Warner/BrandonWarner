e4f95c9b13f7f911990abc94b30470e2ca640ea6
Brandon Warner <brandonswarner5@gmail.com>
2025-05-26 15:08:56 -0400

    fixed webpack loading error on articles .mdx files

---
+++ spotlight-js/next.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  images: {
+    domains: [
+      `${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.${process.env.NEXT_PUBLIC_AWS_REGION}.amazonaws.com`,
+      's3.amazonaws.com',
+    ],
+    // or for newer Next.js versions:
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.s3.amazonaws.com',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mdx$/,
+      use: '@mdx-js/loader',
+    })
+    return config
+  },
+}
--- spotlight-js/src/app/photography/page.jsx
+++ spotlight-js/src/app/photography/page.jsx
@@ -34,15 +34,15 @@ const photosWithUrl= await Promise.all(
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-4 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-         {photosWithUrl.map((photo, index) => {
+         {/* {photosWithUrl.map((photo, index) => {
           console.log('photo: ', photo);
           <Image
           key={photo.Key}
           alt={photo.title}
           src={photo.url}
           className="col-span-2 col-start-2 max-h-33vw w-full object-contain sm:col-start-auto lg:col-span-1"
-        />
-        })} 
+        /> */}
+        {/* })}  */}
           
         </div>
       </div>
