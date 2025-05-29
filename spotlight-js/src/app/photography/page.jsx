ed1f0c26d7063f493ad19ae16caf544d3cdd9633
Brandon Warner <brandonswarner5@gmail.com>
2025-05-28 20:19:17 -0400

    changed photo grid sizing for sm md lg screen size

--- spotlight-js/src/app/photography/page.jsx
+++ spotlight-js/src/app/photography/page.jsx
@@ -22,9 +22,9 @@ const photosWithUrl= await Promise.all(
 );
 
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="bg-white py-24 sm:py-32 dark:bg-zinc-800" >
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-4 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+        <div className="mx-auto grid max-w-lg grid-cols-3 items-center gap-x-8 gap-y-12 md:grid-cols-2 sm:max-w-xl sm:grid-cols-1 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-3">
         {photosWithUrl.map((photo, index) => (
           <Image
             key={photo.Key}
@@ -50,8 +50,8 @@ export const metadata = {
 export default function Uses() {
   return (
     <SimpleLayout
-      title="Software I use, gadgets I love, and other things I recommend."
-      intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
+      title="Photos"
+      intro="Photography has become a new hobby while traveling - here are my favorites from Mexico City, San Francisco, Italy, and New York!"
     >
       <PhotoGallery />
       
