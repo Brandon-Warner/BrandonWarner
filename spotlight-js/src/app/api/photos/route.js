1edcafafe877b8f7e2235f5f2085212781d91e12
Brandon Warner <brandonswarner5@gmail.com>
2025-06-06 17:05:51 -0400

    better photo rendering

--- spotlight-js/src/app/api/photos/route.js
+++ spotlight-js/src/app/api/photos/route.js
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server'
 import { listPhotos, getPhotoUrl } from '../../../utils/s3'
 
+export const dynamemic = 'force-dynamic'
+
 export async function GET(request) {
   try {
     const { searchParams } = new URL(request.nextUrl.searchParams)
