3d1eba15af599a121587ddae0689d0b7c0e3a978
Brandon Warner <brandonswarner5@gmail.com>
2025-06-07 10:37:36 -0400

    error causing mdx files to not load properly, consolidated to next.config.mjs

--- spotlight-js/next.config.mjs
+++ spotlight-js/next.config.mjs
@@ -1,19 +1,30 @@
 import rehypePrism from '@mapbox/rehype-prism'
-import nextMDX from '@next/mdx'
+import createMDX from '@next/mdx'
 import remarkGfm from 'remark-gfm'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  experimental: {
-    outputFileTracingIncludes: {
-      '/articles/*': ['./src/app/articles/**/*.mdx'],
-    },
+  trailingSlash: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: [
+      `${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.${process.env.NEXT_PUBLIC_AWS_REGION}.amazonaws.com`,
+      's3.amazonaws.com',
+    ],
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.s3.amazonaws.com',
+      },
+    ],
   },
 }
 
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
+const withMDX = createMDX({
   options: {
     remarkPlugins: [remarkGfm],
     rehypePlugins: [rehypePrism],
